a5151ccefe00bbbb49ada63d0ff92289307df1e6
rattapum123 <110450506+YoNitichai@users.noreply.github.com>
2024-08-23 11:31:10 +0700

    loop

---
+++ ep5/index.ts
@@ -0,0 +1,17 @@
+//let sum:number=0
+//let isBreak:boolean=false
+//let row:number=0
+
+//while(true){
+    //console.log('วิพูลวิลล่าโสด')
+    //row++
+    //if(row === 3){
+        
+    //}
+//}
+
+// const cars : string[] = ['Honda','Yamaha','Toyota','Suzuki','Mazda']
+
+// cars.forEach(function(c){
+    //console.log(c)
+// })
